5df07edcad5b9bd6915caa1aee0701ed0948b3a4
Tugba Kara <vtugbakara@hotmail.com>
2021-08-03 11:24:37 +0300

    humanDetection.py has been added.

--- computerVisionwithROS/humanDetection.py
+++ computerVisionwithROS/humanDetection.py
@@ -1,87 +1,52 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 import rospy
 import cv2
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError
 import sys #It lets us access system-specific parameters and functions.
-
-class FaceRecognation():
-    def __init__(self):
-        # ROS node is created.
-        self.nodeName = 'imageConverter'
-        rospy.init_node(self.nodeName,anonymous = True)
-        # Subscriber is created for the images that are coming from turtlebot3 itself or usb camera and the images 
-        # coming from different environment are also specified with different topics. e.g turtlebot3 waffle : "/camera/rgbimage_raw/compressed" , usb camera : "/usb_cam/
-        # image_raw"
-        topicAdress = '/usb_cam/image_raw'
-        imageSubscriber = rospy.Subscriber(topicAdress,Image,imageCallback)
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# bridge object is created to make transition of image formats between openCV and ROS.
-bridge = CvBridge()
-
-def imageCallback(ROSimage):
-    print("Image is captured.")
-    global bridge
-    # Bridge object is used to make converition ROS image into openCV image.
-    try:
-        cvImage = bridge.imgmsg_to_cv2(ROSimage,"bgr8")
-        faceDetection(cvImage)
-    except CvBridgeError as e:
-        print(e)
-    
-def faceDetection(cvImage):
-    trainedClassifierFace = cv2.CascadeClassifier(r'/home/tugbakara/desktop/haarcascade_frontalface_default.xml')
-    while cvImage.isOpened():
-        cvImageG = cv2.cvtColor(cvImage,cv2.COLOR_BGR2GRAY)
-        detectedHumans = trainedClassifierFace.detectmultiScale(cvImageG,2,2)
-        for (x,y,w,h) in detectedHumans:
-            roicvImage = cvImage[y:y+h,x:x+h]
-            roicvImageG = cvImageG[y:y+w,x:x+w]
-        cv2.imshow(-)
-
-
-
+class FaceRecognation():   
+    def __init__(self,nodeName,topicAdress):
+        rospy.init_node(nodeName,anonymous = True)
+        # bridge object is created to make transition of image formats between openCV and ROS.
+        self.bridge = CvBridge()
+        rospy.Subscriber(topicAdress,Image,self.imageCallback,queue_size = 1)
+        rospy.loginfo("Image has been captured.")
+    def imageCallback(self,rosImage):
+        try:
+            #ROS Image has been translated into CV image format.
+            cvImage = self.bridge.imgmsg_to_cv2(rosImage,'bgr8')
+        except CvBridgeError as e:
+            print(e)
+        processedImage = self.determination(cvImage)
+        cv2.imshow("Recognized Image",processedImage)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            rospy.signal_shutdown("ROS is shutting down...")
+    def determination(self,cvImage):
+        grayImage = cv2.cvtColor(cvImage,cv2.COLOR_BGR2GRAY)
+        trainedFaceCascade = cv2.CascadeClassifier("/home/tugbakara/Desktop/Dersler/git/pythonExercises/openCV/trainedClassfiers/haarcascade_frontalface_default.xml")
+        recognizedFaces = trainedFaceCascade.detectMultiScale(grayImage,
+                                                                scaleFactor = 1.1, # proportion for lessening image size
+                                                                minNeighbors = 5, # number of neighbor rectangle 
+                                                                minSize = (30,30)) # in object size
+        for(x,y,w,h) in recognizedFaces:
+            cv2.rectangle(cvImage,(x,y),(x+w,y+h),(0,255,255),2)
+        return cvImage      
+        
 def main(args):
     try:
-        FaceRecognation()
+        # Subscriber is created for the images that are coming from turtlebot3 itself or usb camera and the images 
+        # coming from different environment are also specified with different topics.
+        # e.g turtlebot3 waffle : "/camera/rgbimage_raw/compressed",# usb camera : "/usb_cam/image_raw"
+        nodeName = input("Enter the node name :")
+        print("Your noed name is : {}".format(nodeName))
+        topicAdress = input('Enter the adress of the topic to use for Image :')
+        print("Your topic adress is : {} ".format(topicAdress))
+        #For the process created class is called.
+        FaceRecognation(nodeName,topicAdress)
         rospy.spin()
     except KeyboardInterrupt:
-        print("Shutting down!")
+        print("Closing...")
         cv2.destroyAllWindows()
-
 if __name__ == '__main__':
     main(sys.argv)
